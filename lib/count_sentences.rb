46c0079cc440117993a54b60149e6e54b574586b
Brian Diaz <bidiaz101@gmail.com>
2021-10-18 08:53:25 -0500

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match?(/[.]\z/)
   end
 
   def question?
-
+    self.match?(/[?]\z/)
   end
 
   def exclamation?
-
+    self.match?(/[!]\z/)
   end
 
   def count_sentences
-
+    self.split(/[.!?]/)
+    .reject {|str| str.include?("."||"?"||"!")}
+    .reject {|str| str.empty?}
+    .length
   end
 end
